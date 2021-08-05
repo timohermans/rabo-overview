e62c65506623c193e5969d54942188391546fe7c
Timo Hermans <timo.hermans@kabisa.nl>
2021-08-05 21:17:32 +0200

    Check if .env file exists

--- homebudget/settings.py
+++ homebudget/settings.py
@@ -20,7 +20,9 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(path.join(BASE_DIR, '.env'))
+env_file = path.join(BASE_DIR, '.env')
+if path.isfile(env_file):
+    environ.Env.read_env(env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
