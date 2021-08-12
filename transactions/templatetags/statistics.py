f7851f77fcb78221260f39da04bf35932d1145f2
Timo Hermans <timo.hermans@kabisa.nl>
2021-08-12 12:48:21 +0200

    Add empty file for transactions filters

---
+++ transactions/templatetags/statistics.py
@@ -0,0 +1,9 @@
+from transactions.models import Transaction
+from django import template
+from typing import List
+
+register = template.Library()
+
+@register.filter
+def top_expenses(transactions: List[Transaction]):
+	pass
