48d2be17a8120ede0c33f5e0de4e17da387c9074
Timo Hermans <tmhermans@gmail.com>
2021-08-30 14:54:34 +0200

    Setup frontend testing

---
+++ frontend/src/apps/monthly/App.spec.tsx
@@ -0,0 +1,10 @@
+import {render, screen} from "@testing-library/react";
+import App from "./App";
+
+describe("App", () => {
+  it("runs test suite", () => {
+    render(<App />);
+
+    expect(screen.getByText("Hello Vite + React!")).toBeInTheDocument();
+  });
+});
