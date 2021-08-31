1b6675a5924cfff813d71147654d60b7f18e7e16
Timo Hermans <timo.hermans@kabisa.nl>
2021-08-31 20:44:31 +0200

    Add component support through slippers

--- homebudget/settings.py
+++ homebudget/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'transactions.apps.TransactionsConfig'
+    'transactions.apps.TransactionsConfig',
+    'slippers'
 ]
 
 MIDDLEWARE = [
@@ -75,6 +76,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': ['slippers.templatetags.slippers']
         },
     },
 ]
