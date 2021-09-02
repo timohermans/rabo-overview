2de0de41f87fa7e0e4e7d072b1fdb17503ade941
Timo Hermans <timo.hermans@kabisa.nl>
2021-09-02 21:18:32 +0200

    Start working on stats per receiver

--- apps/transactions/templatetags/filters.py
+++ apps/transactions/templatetags/filters.py
@@ -1,29 +1,47 @@
-from apps.transactions.models import Account
 from datetime import date
-from typing import Any, Dict, List
+from typing import Iterator, List
 
 from dateutil.relativedelta import relativedelta
 from django import template
-from django.db.models.base import Model
-from django.forms.models import model_to_dict
+
+from apps.transactions.models import Account, Transaction
 
 register = template.Library()
 
 
 @register.filter
 def previous_month(source: date) -> date:
+    """date - 1"""
     return date(source.year, source.month, 1) - relativedelta(months=1)
 
 
 @register.filter
 def next_month(source: date) -> date:
+    """date + 1"""
     return date(source.year, source.month, 1) + relativedelta(months=1)
 
 
 @register.filter
 def to_date_string(source: date) -> str:
+    """date string for month hrefs"""
     return source.isoformat()
 
+
 @register.filter
-def filter_receivers(accounts: List[Account]) -> List[Account]:
-    return [a for a in accounts if a.is_user_owner == True]
+def receivers(accounts: List[Account]) -> Iterator[Account]:
+    """pulls out receivers from all accounts"""
+    return (a for a in accounts if a.is_user_owner is True)
+
+
+@register.filter
+def short_account_number(account_number: str) -> str:
+    """long IBANs are way too hard to read"""
+    return f"{account_number[:2]}...{account_number[-4:]}"
+
+
+@register.filter
+def of_receiver(
+    transactions: List[Transaction], receiver: Account
+) -> Iterator[Transaction]:
+    """returns transactions of a user owned account"""
+    return (t for t in transactions if t.receiver == receiver)
--- apps/transactions/tests/filters/test_filters.py
+++ apps/transactions/tests/filters/test_filters.py
@@ -1,5 +1,5 @@
 from apps.transactions.models import Account
-from apps.transactions.templatetags.filters import filter_receivers
+from apps.transactions.templatetags import filters
 
 
 def test_receivers_filters_receivers_from_accounts() -> None:
@@ -10,8 +10,20 @@ def test_receivers_filters_receivers_from_accounts() -> None:
         Account(name="Beter Bed", account_number="123456", is_user_owner=False),
     ]
 
-    receivers = filter_receivers(accounts)
+    receiver_list = list(filters.receivers(accounts))
 
-    assert len(receivers) == 2
-    assert len([r for r in receivers if r.name == "Betaalrekening"]) == 1
-    assert len([r for r in receivers if r.name == "Spaarrekening"]) == 1
+    assert len(receiver_list) == 2
+    assert len([r for r in receiver_list if r.name == "Betaalrekening"]) == 1
+    assert len([r for r in receiver_list if r.name == "Spaarrekening"]) == 1
+
+
+def test_short_account_number_displays_last_characters() -> None:
+    account_number = "NL11RABO104389104"
+
+    actual = filters.short_account_number(account_number)
+
+    assert actual == "NL...9104"
+
+
+def test_of_receiver_filters_transactions_for_receiver() -> None:
+    raise NotImplementedError()
